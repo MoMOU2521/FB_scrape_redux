d62698979d1ae60f19d134a32bafd11c2d4f88e1
aasava <ama.2522@proton.me>
2026-09-22 16:56:59 +0700

    refactor deterministic stop1.2

--- db/services/scraper/get_last_scraped_post.py
+++ db/services/scraper/get_last_scraped_post.py
@@ -1,17 +1,17 @@
-# db.services.scraper.get_last_scraped_post.py
-from db.database import db
+# # db.services.scraper.get_last_scraped_post.py
+# from db.database import db
 
 
-def get_last_scraped_post(group_name: str):
-    row = db.cur.execute(
-        """
-        SELECT group_name, post_id
-        FROM posts
-        WHERE group_name = ?
-        ORDER BY scraped_at DESC
-        LIMIT 1
-        """,
-        (group_name,),
-    ).fetchone()
+# def get_last_scraped_post(group_name: str):
+#     row = db.cur.execute(
+#         """
+#         SELECT group_name, post_id
+#         FROM posts
+#         WHERE group_name = ?
+#         ORDER BY scraped_at DESC
+#         LIMIT 1
+#         """,
+#         (group_name,),
+#     ).fetchone()
 
-    return dict(row) if row else None
+#     return dict(row) if row else None
